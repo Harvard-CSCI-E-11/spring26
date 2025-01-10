9c4a9d14960fa4a17836acad017b31929bb0df83
Simson Garfinkel <simsong@acm.org>
2025-01-09 21:11:12 -0500

    works with Http API, custom domain, Flask and gunicorn

--- staff/leaderboard/leaderboard_app/app.py
+++ staff/leaderboard/leaderboard_app/app.py
@@ -4,4 +4,4 @@ Lambda handler for AWS API Gateway
 
 from apig_wsgi import make_lambda_handler
 import leaderboard_app
-lambda_handler = make_lambda_handler(leaderboard_app.app)
+app = make_lambda_handler(leaderboard_app.app)
