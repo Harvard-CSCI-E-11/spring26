605ca429e36206b9811a2abe0d53d44ee764523b
Simson L. Garfinkel <simsong@acm.org>
2025-02-22 14:36:48 -0500

    Create simple_flask_application.py

---
+++ lab3/simple_flask_application.py
@@ -0,0 +1,15 @@
+### app.py ###
+# Start of the flask application
+#
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello from Flask!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
+
+################################################
