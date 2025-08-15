5fe98b739429a9451fae0bd8a932c682dc281813
Simson Garfinkel <simsong@acm.org>
2025-08-15 00:03:07 +0000

    updated the lab3 files for ubuntu

--- lab3/etc/setup-certbot-crontab.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-sudo yum install cronie -y
-sudo systemctl enable crond.service
-sudo systemctl start crond.service
-sudo systemctl status crond | grep Active
-sudo systemctl status crond.service
-echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && sudo certbot renew -q" | sudo tee -a /etc/crontab  > /dev/null
