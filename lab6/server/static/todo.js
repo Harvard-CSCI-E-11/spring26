9fcebf17554338bc0b1a5203e936372611d3dab3
Simson Garfinkel <simsong@acm.org>
2025-10-29 12:00:04 +0900

    update

--- lab6/server/static/todo.js
+++
@@ -1,8 +0,0 @@
-def celebrities_to_html(celebs):
-    html = ''
-    for celeb in celebs:
-        html += f"Name: {celeb.get('Name')} Confidence: {celeb.get('MatchConfidence'):2.4}% "
-        for url in celeb.get('Urls', []):
-            html += f"<a target='_' href='https://{url}'>[link]</a>"
-        html += "<br/>\n"
-    return html
