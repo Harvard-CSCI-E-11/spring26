e811b9e5ddadb158c9a427d8e1f246f7fc8b6731
Simson Garfinkel <simsong@acm.org>
2025-11-14 17:49:25 -0800

    update

--- etc/e11-cli/tests/test_e11_command.py
+++ etc/e11-cli/tests/test_e11_command.py
@@ -1,10 +1,10 @@
 import os
 
 import e11
-import e11.__main__
+import e11.main
 
 def test_patched_config(_isolate_env):
-    cp = e11.__main__.get_config()
+    cp = e11.main.get_config()
     assert cp['student']['email'] == 'test@example.org' # from conftest.py
     assert cp['student']['smashedemail'] == 'testexampleorg' # from conftest.py
     assert cp['student']['public_ip'] == '127.0.0.1' # from conftest.py
