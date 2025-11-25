2d6666e61adb949f98406e1840f07a4a50be4845
Simson Garfinkel <simsong@acm.org>
2025-11-25 15:13:24 -0500

    removed needless else

--- etc/e11-cli/lambda-home/src/home_app/home.py
+++ etc/e11-cli/lambda-home/src/home_app/home.py
@@ -540,8 +540,7 @@ def api_register(event, payload):
                        preferred_name=user.preferred_name))
         add_user_log(event, user.user_id, f'Registration email sent to {email}')
         return resp_json(200,{'message':f'DNS updated and email sent successfully. new_records={new_records} changed_records={changed_records}'})
-    else:
-        return resp_json(200,{'message':f'DNS updated. No email sent. new_records={new_records} changed_records={changed_records}'})
+    return resp_json(200,{'message':f'DNS updated. No email sent. new_records={new_records} changed_records={changed_records}'})
 
 
 def api_heartbeat(event, context):
